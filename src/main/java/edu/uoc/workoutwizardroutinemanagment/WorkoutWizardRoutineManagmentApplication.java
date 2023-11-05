da2b0ce6f7bc5d10a2bf880610c9fc2e6bb305df
pablocanohueros <canohueros75@gmail.com>
2023-11-05 19:13:48 +0100

    init

---
+++ src/main/java/edu/uoc/workoutwizardroutinemanagment/WorkoutWizardRoutineManagmentApplication.java
@@ -0,0 +1,13 @@
+package edu.uoc.workoutwizardroutinemanagment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkoutWizardRoutineManagmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkoutWizardRoutineManagmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/uoc/workoutwizardroutinemanagment/WorkoutWizardRoutineManagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.uoc.workoutwizardroutinemanagment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutWizardRoutineManagmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
