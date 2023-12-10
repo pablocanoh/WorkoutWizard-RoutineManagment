76563d7b6721cf0f8f92eabe555286f915623003
pablocanohueros <canohueros75@gmail.com>
2023-12-10 15:52:27 +0100

    Add initial gradle wrapper scripts and configurations
    This commit introduces initial gradle wrapper scripts and configurations for 'routine-client' and 'routine-core' modules. These scripts will be used for building and running the modules. The '.gitignore' files have been updated to ignore build artifacts across different IDEs. This will help to prevent unnecessary files from being tracked in the Git version control. Furthermore, an empty 'application.properties' file was added to the 'routine-client' resource directory as a placeholder for future configurations. Last but not least, the build.gradle scripts in the 'routine-core' and 'routine-client' modules define the gradle project properties, plugins added, and its dependencies required for build and test cases.

---
+++ routine-client/src/main/java/com/example/routineclient/RoutineClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.routineclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoutineClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoutineClientApplication.class, args);
+    }
+
+}
---
+++ routine-client/src/test/java/com/example/routineclient/RoutineClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.routineclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoutineClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
