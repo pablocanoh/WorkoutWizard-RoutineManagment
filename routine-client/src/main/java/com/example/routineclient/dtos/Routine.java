bb7b74a35ca069a8fac3ce816a51eb61400f32d6
pablocanohueros <canohueros75@gmail.com>
2023-12-10 20:15:06 +0100

    Add getDays method to Routine class
    A method getDays was added to the Routine class. This method allows for retrieving the number of days a routine contains by returning the size of the list of RoutineDay instances associated with this routine. This ensures that we have a streamlined manner to acquire this information when necessary, rather than manually computing it every time.

--- routine-client/src/main/java/com/example/routineclient/dtos/Routine.java
+++ routine-client/src/main/java/com/example/routineclient/dtos/Routine.java
@@ -3,4 +3,9 @@ package com.example.routineclient.dtos;
 import java.util.List;
 import java.util.UUID;
 
-public record Routine(UUID id, List<RoutineDay> blocks) { }
+public record Routine(UUID id, List<RoutineDay> blocks) {
+
+    public int getDays() {
+        return blocks.size();
+    }
+}
