e4313db771c96b0b9cf1de98c960d5b26057aacb
pablocanohueros <canohueros75@gmail.com>
2023-12-23 22:53:41 +0100

    Update database id type and modify DB configurations
    The commit includes changes inplace to sort database inconsistencies and improve code maintainability. The id type in the entities 'Routine', 'RoutineDay' and 'ExerciseWithReps' were changed from generic data types to UUID to enhance uniqueness and reduce the chance for errors. Docker configuration and application properties have been modified to connect to a new database 'routineDB'.
    
    The 'JoinColumn' in 'RoutineDay' object was refactored from 'exercise_id' to 'routine_day_id' to avoid confusion and improve code readability. Also, in 'RoutineService', a new UUID is generated for each exercise for uniqueness.

--- routine-client/src/main/java/com/example/routineclient/RoutineClient.java
+++ routine-client/src/main/java/com/example/routineclient/RoutineClient.java
@@ -1,12 +1,19 @@
 package com.example.routineclient;
 
+import com.example.routineclient.dtos.ExerciseResponse;
+import com.example.routineclient.dtos.ExerciseType;
+import com.example.routineclient.dtos.ExperienceLevel;
 import com.example.routineclient.dtos.Routine;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.UUID;
 
 public class RoutineClient {
@@ -29,6 +36,69 @@ public class RoutineClient {
 
             assert response.body() != null;
             return objectMapper.readValue(response.body().byteStream(), Routine.class);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Routine suggest(ExperienceLevel experienceLevel, int daysPerWeek) {
+        final var request = new Request.Builder()
+                .url(baseUrl + "/routine/suggest?experienceLevel=" + experienceLevel + "&daysPerWeek=" + daysPerWeek)
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+
+            assert response.body() != null;
+            return objectMapper.readValue(response.body().byteStream(), Routine.class);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public UUID create(Routine routine) throws JsonProcessingException {
+        final var request = new Request.Builder()
+                .url(baseUrl + "/routine")
+                .post(okhttp3.RequestBody.create(
+                        objectMapper.writeValueAsString(routine),
+                        okhttp3.MediaType.parse("application/json")))
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+
+            assert response.body() != null;
+            return objectMapper.readValue(response.body().byteStream(), UUID.class);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Map<ExerciseType, List<ExerciseResponse>> getExercise() {
+        final var request = new Request.Builder()
+                .url(baseUrl + "/routine/exercise")
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+
+            assert response.body() != null;
+            return objectMapper.readValue(response.body().byteStream(), Map.class);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Optional<Routine> getLatest() {
+        final var request = new Request.Builder()
+                .url(baseUrl + "/routine/latest")
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+
+            assert response.body() != null;
+            return Optional.ofNullable(objectMapper.readValue(response.body().byteStream(), Routine.class));
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
